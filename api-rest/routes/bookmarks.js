1e0d58ff5b1d8e02253d28e033052c74b240b153
Marcus Couto <marcus.couto@stormsec.com.br>
2017-04-09 18:05:56 -0300

    Atualização das rotas de users e bookmarks

---
+++ api-rest/routes/bookmarks.js
@@ -0,0 +1,51 @@
+module.exports = app => {
+	// Get data from Models
+	const Bookmarks = app.db.models.Bookmarks;
+	app.route("/bookmarks")
+		.get((req, res) => {
+			Bookmarks.findAll({})
+			.then(result => res.json({bookmarks: bookmarks}))
+			.catch(error => {
+				res.status(412).json({msg: error.message});
+			});
+		})
+		.post((req, res) => {
+			Bookmarks.create(req.body)
+			.then(result = res.json(result))
+			.catch(error => {
+				res.status(412).json({msg: error.message});
+			});
+
+		});
+	
+	app.route("/bookmarks/:id")
+		.get((req, res) => {
+			Bookmarks.findOne({where: req.params})
+			.then( result => {
+				if(result){
+					res.json(result);
+				}else{
+					res.sendStatus(404);
+				}
+			})
+			.catch(error => {
+				res.status(412).json({msg: error.message});
+			});
+
+		})
+		.put((req, res) => {
+			Bookmarks.update(req.body, {where: req.params})
+			.then(result => res.sendStatus(204))
+			.catch(error => {
+				res.status(412).json({msg: error.message})
+			});
+
+		})
+		.delete((req, res) => {
+			Bookmarks.destroy({where: req.params})
+			.then(result => res.sendStatus(204))
+			.catch(error =>{
+				res.status(412).json({msg: error.message})
+			});
+		});
+};
--- api-rest/routes/users.js
+++ api-rest/routes/users.js
@@ -1,59 +1,31 @@
 module.exports = app => {
-	// Get data from Models
-	const Users = app.db.models.users;
-	app.route("/users")
-		.all((req, res) => {
-			delete req.body.id;
-			next();
-		})
-		.get((req, res) => {
-			Users.findAll({})
-			.then(result => res.json({users: users}))
-			.catch(error => {
-				res.status(412).json({msg: error.message});
-			});
-		})
-		.post((req, res) => {
-			Users.create(req.body)
-			.then(result = res.json(result))
-			.catch(error => {
-				res.status(412).json({msg: error.message});
-			});
-
-		});
+	const Users.app.db.models.Users;
 	
-	app.route("/users/:id")
-		.all((req, res) => {
-			delete req.body.id;
-			next();
+	app.get("/users/:id", (req, res) => {
+		Users.findById(req.params.id, {
+			attributes: ["id", "name", "permission"]
 		})
-		.get((req, res) => {
-			Users.findOne({where: req.params})
-			.then( result => {
-				if(result){
-					res.json(result);
-				}else{
-					res.sendStatus(404);
-				}
-			})
-			.catch(error => {
-				res.status(412).json({msg: error.message});
-			});
-
-		})
-		.put((req, res) => {
-			Users.update(req.body, {where: req.params})
-			.then(result => res.sendStatus(204))
-			.catch(error => {
-				res.status(412).json({msg: error.message})
-			});
-
-		})
-		.delete((req, res) => {
-			Users.destroy({where: req.params})
-			.then(result => res.sendStatus(204))
-			.catch(error =>{
-				res.status(412).json({msg: error.message})
-			});
+		.then(result => res.json(result))
+		.catch(error => {
+			res.status(412).json({msg: error.message})
 		});
-};
+	});
+
+	app.delete("/users/:id", (req, res) => {
+		Users.destroy({where: {id: req.params.id}})
+		.then(result => res.sendStatus(204))
+		.catch(error => {
+			res.status(412).json({msg: error.message})
+		});
+	});
+
+	app.post("/users", (req, res) =>{
+		Users.create(req.body)
+		.then(result = res.json(result))
+		.catch(error => {
+			res.status(412).json({msg: error.message})
+		});
+	});
+
+	
+}
