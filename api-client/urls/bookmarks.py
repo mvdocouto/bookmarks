8751c92ccb04d2f7a339cf6b70a3d238f459db81
Marcus Couto <marcus.couto@stormsec.com.br>
2017-04-12 23:30:52 -0300

    Criação da client do CRUD bookmark

--- api-client/urls/bookmarks.py
+++ api-client/urls/bookmarks.py
@@ -1,30 +1,72 @@
 #-*- coding: utf-8 -*-
 """View."""
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect, url_for
+from flask import request
 from jinja2 import TemplateNotFound
+import simplejson as json
 import requests
 
 bp_bookmarks = Blueprint('bookmarks', __name__,
                         template_folder='templates')
 
-
 @bp_bookmarks.route('/',methods=['GET'])
 def list_bookmarks():
-	return render_template('bookmarks/index.html') 
+	result = requests.get("http://localhost:3000/bookmarks")
+	context = {"bookmarks": result.json()}
+	return render_template('bookmarks/index.html', **context), 200 
+ 
+
 
 @bp_bookmarks.route('/add', methods=['GET'])
-def view_bookmark_form():
-	return render_template('bookmarks/form.html') 
+def insert_form():
+	context = {"bookmark":[]}
+	return render_template('bookmarks/form.html', **context), 200 
+ 
 
-@bp_bookmarks.route('/add', methods=['POST'])
-def insert_bookmark():
-	pass 
+@bp_bookmarks.route('/add', methods=['POST']) 
+def insert_user():
+	# session.get('user')
+	# session['username'] = username
+	name = request.form.get("nome") 
+	url = request.form.get("url") 
+	payload = {
+		'name': name, 
+		'url': password,
+		}
+	headers = {'Content-Type': 'application/json'}
+	response = requests.post("http://localhost:3000/bookmarks", 
+		data=json.dumps(payload), headers=headers)
+	if response.status_code == 200:
+		return redirect(url_for('bp_bookmarks.list_bookmarks'))
 
 
 @bp_bookmarks.route('/update/<int:id>', methods=['GET'])
-def update_bookmark():
-	pass 
+def update_form(id):
+	result = requests.get("http://localhost:3000/bookmark/{id}".format(id=id))
+	context = {"user": result.json()}
+	return render_template('bookmarks/form.html', **context), 200 
+ 
+
+@bp_bookmarks.route('/update/<int:id>', methods=['POST'])
+def update_user(id):
+	name = request.form.get("nome") 
+	url = request.form.get("url") 
+	payload = {
+		'name': name, 
+		'url': url,
+		}
+	headers = {'Content-Type': 'application/json'}
+	response = requests.put("http://localhost:3000/bookmark/{id}".format(id=id), 
+		data=json.dumps(payload), headers=headers)
+	print response.status_code
+	if response.status_code == 204:
+		return redirect(url_for('bp_bookmarks.list_bookmarks'))
+
 
 @bp_bookmarks.route('/delete/<int:id>', methods=['GET'])
-def delete_bookmark():
-	pass 
+def delete_user(id):
+	headers = {'Content-Type': 'application/json'}
+	response = requests.delete("http://localhost:3000/bookmark/{id}".format(id=id))
+	if response.status_code == 204:
+		return redirect(url_for('bp_bookmarks.list_bookmarks'))
+		
--- api-client/urls/users.py
+++ api-client/urls/users.py
@@ -1,65 +1,78 @@
 #-*- coding: utf-8 -*-
 """View."""
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect, url_for
+from flask import request
 from jinja2 import TemplateNotFound
+import simplejson as json
 import requests
 
 bp_users = Blueprint('bp_users', __name__,
                         template_folder='templates')
 
 
+
 @bp_users.route('/',methods=['GET'])
 def list_users():
-	r = requests.get('<MY_URI>',headers=('Authorization: TOK:<MY_TOKEN>'))
-	r.json()
-	return render_template('users/index.html') 
+	result = requests.get("http://localhost:3000/users")
+	context = {"users": result.json()}
+	return render_template('users/index.html', **context), 200 
  
 
 @bp_users.route('/add', methods=['GET'])
 def insert_form():
-	return render_template('users/form.html') 
+	context = {"user":[]}
+	return render_template('users/form.html', **context), 200 
  
 @bp_users.route('/add', methods=['POST']) 
 def insert_user():
-	session.get('user')
-	session['username'] = username
-	name = request.values.get("name") 
-	password = request.values.get("password") 
-	email = request.values.get("email") 
-	permission = request.values.get("permission") 
-	data = {
+	# session.get('user')
+	# session['username'] = username
+	# import ipdb; ipdb.set_trace()
+	name = request.form.get("nome") 
+	password = request.form.get("password") 
+	email = request.form.get("email") 
+	permission = request.form.get("permission") 
+	payload = {
 		'name': name, 
 		'password': password,
 		'email': email,
 		'permission': permission
 		}
-	r = requests.post("http://localhost:3000/user", data=data)
-	print r.text
-	return render_template('users/form.html') 
- 
-
+	headers = {'Content-Type': 'application/json'}
+	response = requests.post("http://localhost:3000/users", data=json.dumps(payload), headers=headers)
+	if response.status_code == 200:
+		return redirect(url_for('bp_users.list_users'))
 
 @bp_users.route('/update/<int:id>', methods=['GET'])
 def update_form(id):
-	result = requests.get("http://localhost:3000/user")
-	return render_template('users/form.html', **result) 
-
-@bp_users.route('/update/<int:id>', methods=['GET'])
-def update_user():
-	name = request.values.get("name") 
-	password = request.values.get("password") 
-	email = request.values.get("email") 
-	permission = request.values.get("permission") 
-	data = {
+	result = requests.get("http://localhost:3000/user/{id}".format(id=id))
+	context = {"user": result.json()}
+	return render_template('users/form.html', **context), 200 
+ 
+@bp_users.route('/update/<int:id>', methods=['POST'])
+def update_user(id):
+	name = request.form.get("nome") 
+	password = request.form.get("password") 
+	email = request.form.get("email") 
+	permission = request.form.get("permission")
+	payload = {
 		'name': name, 
-		'password': password,
 		'email': email,
 		'permission': permission
 		}
-	r = requests.put("http://localhost:3000/user/{id}".format(id=id), data=data)
-	pass 
+	if password:
+		payload['password'] = password
+
+	headers = {'Content-Type': 'application/json'}
+	response = requests.put("http://localhost:3000/user/{id}".format(id=id), data=json.dumps(payload), headers=headers)
+	print response.status_code
+	if response.status_code == 204:
+		return redirect(url_for('bp_users.list_users'))
 
 @bp_users.route('/delete/<int:id>', methods=['GET'])
 def delete_user(id):
-	r = requests.delete("http://localhost:3000/user/{id}".format(id=id), data=payload)
-	pass 
+	headers = {'Content-Type': 'application/json'}
+	response = requests.delete("http://localhost:3000/user/{id}".format(id=id))
+	if response.status_code == 204:
+		return redirect(url_for('bp_users.list_users'))
+		
